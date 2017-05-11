25d87a6e9b423eef6aac65cd818c45aa79480ac5
Lur Ibargutxi <libargutxi@codesyntax.com>
2017-05-11 15:58:33 +0200

    DNI, NIF and NIE validator

--- cs/pfg/dnivalidator/dni.py
+++ cs/pfg/dnivalidator/dni.py
@@ -20,16 +20,31 @@ class Dni:
         self.description = description
 
     def __call__(self, value, *args, **kwargs):
-        if value[0] not in ['0','1','2','3','4','5','6','7','8','9']:
-            value=value[1:]
-        let = value[-1:].upper()
-	number = value[:-1]
-	number = int(number)
-	number = number % 23
-	letters = 'TRWAGMYFPDXBNJZSQVHLCKET'
-	letter = letters[number:number + 1]
-	if let == letter:
-            return 1
+        tabla = "TRWAGMYFPDXBNJZSQVHLCKE"
+        dig_ext = "XYZ"
+        reemp_dig_ext = {'X': '0', 'Y': '1', 'Z': '2'}
+        numeros = "1234567890"
+        dni = dni.upper()
+        if len(dni) == 9:
+            dig_control = dni[8]
+            dni = dni[:8]
+            if dni[0] in dig_ext:
+                dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
+            if len(dni) == len([n for n in dni if n in numeros]) and tabla[int(dni) % 23] == dig_control:
+                return 1
         return ("Validation failed(%s): must be checked." % self.name)
 
-validation.register(Dni('dni')) 
+    # def __call__(self, value, *args, **kwargs):
+    #     if value[0] not in ['0','1','2','3','4','5','6','7','8','9']:
+    #         value=value[1:]
+    #     let = value[-1:].upper()
+    #     number = value[:-1]
+    #     number = int(number)
+    #     number = number % 23
+    #     letters = 'TRWAGMYFPDXBNJZSQVHLCKET'
+    #     letter = letters[number:number + 1]
+    #     if let == letter:
+    #         return 1
+    #     return ("Validation failed(%s): must be checked." % self.name)
+
+validation.register(Dni('dni'))
